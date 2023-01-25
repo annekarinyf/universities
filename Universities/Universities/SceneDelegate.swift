4889d6e133c0ef31a62b7cdfd62227357d610d75
Anne Kariny Silva Freitas <annekarinyfreitas@gmail.com>
2023-01-25 18:08:51 -0300

    Create project

---
+++ Universities/Universities/SceneDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  SceneDelegate.swift
+//  Universities
+//
+//  Created by Anne Kariny Silva Freitas on 25/01/23.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(frame: .zero)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = ViewController()
+        window?.windowScene = windowScene
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) { }
+    func sceneDidBecomeActive(_ scene: UIScene) { }
+    func sceneWillResignActive(_ scene: UIScene) { }
+    func sceneWillEnterForeground(_ scene: UIScene) { }
+    func sceneDidEnterBackground(_ scene: UIScene) { }
+}
+
---
+++ Universities/Universities/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Universities
+//
+//  Created by Anne Kariny Silva Freitas on 25/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
